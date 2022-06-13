7eff8a52fe1a20f8179cf2f9a5cd9b8e6d16d96e
SaiVenkatDhulipudi <saivenkat952967@gmail.com>
2022-06-13 14:44:54 +0530

    added ispalindrome function

--- api.py
+++ api.py
@@ -1,9 +1,14 @@
-from flask import Flask,request
+from flask import Flask,request,jsonify
 app=Flask(__name__)
+
+helper=dict()
+helper["iseven/int"]="returns if a number is even or not"
+helper["isprime/int"]="returns if a number is prime or not"
 
 @app.route('/')
 def home():
-    return "hello world"
+    return jsonify(helper) 
+
 @app.route('/iseven/<int:n>')
 def iseven(n):
     if not n&1:
@@ -21,7 +26,19 @@ def isprime(n):
     for i in range(5,int(n**(0.5))+1,6):
         if n%i==0:
             return '{} is not prime'.format(n)
-    return '{} is  prime'.format(n)
+    return '{} is  prime'.format(n) 
+@app.route('/ispalindrome/<int:n>')
+def ispali(n):
+    c=0
+    k=n
+    while k:
+        c=c*10
+        c+=k%10
+        k=k//10
+    if c==n:
+        return "{} is palindrome".format(n)
+    else:
+        return "{} is not palindrome".format(n)
 
 if __name__=='__main__':
     app.run(debug=True)
